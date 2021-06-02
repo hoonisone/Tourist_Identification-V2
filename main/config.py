dcdc25c3f2c9a3295613482cab4e7101e621a873
Myounghun <hoonisone@gmail.com>
2021-06-02 13:00:42 +0900

    take off access station names

--- main/config.py
+++ main/config.py
@@ -11,3 +11,7 @@ STATION_DF_PATH = "data/analysis/station_df.csv"
 USER_DF_PATH = "data/analysis/user_df.csv"
 TOUR_STATION_CANDIDATE_DF_PATH = "data/analysis/tour_station_candidate_df.csv"
 USAGE_DF_PATH = "data/analysis/usage_df.csv"
+
+# 공항과 항만에 대한 이름 후보(정류장 이름의 부분 문자열) 데이터
+airport_candidate_list = ["제주국제공항"]
+harbor_candidate_list = ["국제여객터미널", "제주연안여객터미널", "제4부두", "제6부두", "임항로", "제주해양경찰서"]
