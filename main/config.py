5d5ddc76ebfa883eb888b41167cba76ef245112e
Myounghun <hoonisone@gmail.com>
2021-06-02 12:56:31 +0900

    extract parameter

---
+++ main/config.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+# data range
+START_DATE = datetime.datetime(2019, 6, 1)
+END_DATE = datetime.datetime(2019, 8, 29)
+
+PROJECT_PATH = "./.."
+DEFAULT_ENCODING = "cp949"
+STATION_DF_PATH = "data/analysis/station_df.csv"
+USER_DF_PATH = "data/analysis/user_df.csv"
+TOUR_STATION_CANDIDATE_DF_PATH = "data/analysis/tour_station_candidate_df.csv"
+USAGE_DF_PATH = "data/analysis/usage_df.csv"
